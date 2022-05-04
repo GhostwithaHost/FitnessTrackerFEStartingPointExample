10141bacdb470bfef729bf60a1d89042c381ea60
Shane <shanedfuentes@gmail.com>
2022-05-04 12:56:07 -0500

    formatting basic api interaction bones.

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,16 @@ import React, { useEffect, useState } from 'react';
 import ColorBox from './ColorBox';
 
 const App = () => {
-    // useEffect(async () => {
-    //     console.log("IN HERE")
-    //     const res = await fetch("https://fitnesstrac-kr.herokuapp.com/api/activities")
-    //     // const res = await fetch("heroku-link");
-    //     // const res = await fetch("localhost:3000");
-    //     const json = await res.json();
-    //     // setReq(json);
-    //     console.log(json)
-    //     // return () => { }
-    // }, [])
+    useEffect(async () => {
+        console.log("IN HERE")
+        const res = await fetch("https://fitnesstrac-kr.herokuapp.com/api/activities")
+        // const res = await fetch("heroku-link");
+        // const res = await fetch("localhost:3000");
+        const json = await res.json();
+        // setReq(json);
+        console.log(json)
+        // return () => { }
+    }, [])
 
     return (
         <>
---
+++ src/components/NavBar.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const NavBar = (props) => {
+
+const {isLoggedIn, loggedInUsername} = props;
+
+    return (
+        <div id="navbar">
+            <div id="siteName" className="navtext">
+                Fitness Trackr
+            </div>
+            <div id="siteLinks">
+            {isLoggedIn ?
+                <span className="navtext">{`Welcome, ${loggedInUsername}`}</span>
+                :
+                <span className="navtext">Welcome, Guest</span>
+            }
+
+            <Link to="/">
+                <a className="navtext">HOME</a>
+            </Link>
+
+            <Link to="/posts">
+                <a className="navtext">POSTS</a>
+            </Link>
+
+            {isLoggedIn ?
+                <Link to="/messages">
+                    <a className="navtext">MESSAGES</a>
+                </Link>
+                :
+                null
+            }
+
+            {isLoggedIn ?
+                <Link to="/">
+                    <a className="navtext"
+                        onClick={() => {
+                            localStorage.removeItem("access_token");
+                        }}>LOG OUT</a>
+                </Link>
+                :
+                null}
+        </div>
+        </div>
+    )
+}
+
+export default NavBar;
